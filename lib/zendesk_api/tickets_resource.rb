478065a504a26895a88266b1d8a2b9336ae404ec
Rafael Bandeira <rafaelbandeira3@gmail.com>
2014-02-05 10:21:46 -0600

    Support for show_many

--- lib/zendesk_api/tickets_resource.rb
+++ lib/zendesk_api/tickets_resource.rb
@@ -2,4 +2,8 @@ class ZendeskApi::TicketsResource < ZendeskApi::Resource
   def resource_name
     "ticket"
   end
+
+  def show_many(ids)
+    request(:get, "show_many", ids: ids.join(","))[collection_root_element]
+  end
 end
